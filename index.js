3e92630c7031dbb5fe14bbcc80e064753d17ab3a
mufeng <mufeng.me@gmail.com>
2022-12-31 10:35:05 +0800

    Update pattern regex

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
 const Names = require('./names.json');
+const Patterns = require('./patterns.json');
 const Badwords = require('./badwords.json');
 
 const namesMap = Names.reduce((accumulator, name) => {
     accumulator[name] = true;
     return accumulator;
 }, {});
+
+const patternsRegex = Patterns.map((pattern) => {
+    return new RegExp(pattern);
+});
 
 const badwordsRegex = Badwords.map((badword) => {
     return new RegExp(`\\b${badword.replace(/(\W)/g, '\\$1')}\\b`, 'gi')
@@ -19,6 +24,14 @@ class ReservedSubdomains {
     static isValid(name) {
         if (namesMap.hasOwnProperty(name)) {
             return false;
+        }
+
+        for (let i = 0; i < patternsRegex.length; ++i) {
+            const regex = patternsRegex[i];
+
+            if (regex.test(name)) {
+                return false;
+            }
         }
 
         for (let i = 0; i < badwordsRegex.length; ++i) {
--- index.test.js
+++ index.test.js
@@ -4,10 +4,14 @@ describe('ReservedSubdomains', () => {
   it('catches valid and invalid domains', () => {
     const valid = "this-is-valid";
     const invalid = "www";
+    const invalid2 = "www2"
+
     expect(ReservedSubdomains.isValid(valid)).toBe(true);
     expect(ReservedSubdomains.isValid(invalid)).toBe(false);
+    expect(ReservedSubdomains.isValid(invalid2)).toBe(false);
     expect(ReservedSubdomains.isNotValid(valid)).toBe(false);
     expect(ReservedSubdomains.isNotValid(invalid)).toBe(true);
+    expect(ReservedSubdomains.isNotValid(invalid2)).toBe(true);
   })
 
   it('is case sensitive because it is developer responsibility to lowercase the inputs', () => {
